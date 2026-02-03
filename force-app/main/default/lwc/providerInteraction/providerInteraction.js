0bebb7e3e401801d2d676d17e2fd7e08b5351160
Jay Cina <jcina.triafed@gmail.com>
2026-02-03 16:41:56 -0500

    providerInteraction set up - missing API calls so no Providers are available

--- force-app/main/default/lwc/providerInteraction/providerInteraction.js
+++ force-app/main/default/lwc/providerInteraction/providerInteraction.js
@@ -1,3 +1,52 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Case_Provider__c.Provider_Name__c';
+import PHONE_NUMBER_FIELD from '@salesforce/schema/Case_Provider__c.Phone_Number__c';
+import RATING_FIELD from '@salesforce/schema/Case_Provider__c.Rating__c';
+import SERVICE_CATEGORY_FIELD from '@salesforce/schema/Case_Provider__c.Service_Category__c';
+import SPECIALTY_FIELD from '@salesforce/schema/Case_Provider__c.Specialty__c';
+import getCaseProviders from '@salesforce/apex/Case_Manager.getCaseProviders';
+const COLUMNS = [
+  { label: 'Rating', fieldName: RATING_FIELD.fieldApiName, type: 'text'},
+  { label: 'Provider Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
+  { label: 'Phone Number', fieldName: PHONE_NUMBER_FIELD.fieldApiName, type: 'text'},
+  { label: 'Service Category', fieldName: SERVICE_CATEGORY_FIELD.fieldApiName, type: 'text'},
+  { label: 'Speciality', fieldName: SPECIALTY_FIELD.fieldApiName, type: 'text'}
+];
 
-export default class ProviderInteraction extends LightningElement {}
+export default class ProviderInteraction extends LightningElement {
+  providersData = []
+  sortedProviders = []
+
+  columns = COLUMNS;
+  defaultSortDirection = 'desc';
+  sortDirection = 'desc';
+  sortedBy;
+
+  @wire(getCaseProviders)
+  wiredRequests({ data, error}) {
+    if(data) {
+      this.providersData = data;
+      this.sortedProviders = [...data];
+      console.log('data:', data)
+    }
+    else if(error) {
+      console.error("Wire error:", error);
+    }
+  }; 
+  
+  get hasData() {
+    return this.sortedProviders && this.sortedProviders.length > 0;
+  }
+  
+  get errors() {
+    return this.providersData.error ? reduceErrors(this.providersData.error) : [];
+  }
+
+  onHandleSort(event) {
+
+  }
+
+  handleFindProviders(event) {
+    
+  }
+}
