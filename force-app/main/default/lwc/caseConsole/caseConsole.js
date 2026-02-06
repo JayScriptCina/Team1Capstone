4603174ba6c702f7e6d584fcb36138bd25060c89
Jay Cina <jcina.triafed@gmail.com>
2026-02-06 09:18:33 -0500

    data functions moved to parent caseConsole.js

--- force-app/main/default/lwc/caseConsole/caseConsole.js
+++ force-app/main/default/lwc/caseConsole/caseConsole.js
@@ -1,6 +1,86 @@
 // Name: Jay Cina
 // Date: 02/04/2026
 // Desc: A console which displays a dynamic list (caseList.js) and a details panel if a case is clicked
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 
-export default class CaseConsole extends LightningElement {}
+import CASE_OBJECT from '@salesforce/schema/Case';
+import STATUS_FIELD from '@salesforce/schema/Case.Status';
+import PRIORITY_FIELD from '@salesforce/schema/Case.Priority';
+
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+
+import getCases from '@salesforce/apex/Case_Manager.getCases';
+
+export default class CaseConsole extends LightningElement {
+  @track cases = [];
+  @track statusOptions = [];
+  @track priorityOptions = [];
+  @track recordTypeOptions = [];
+  caseRecordTypeId;
+  
+  // 1. Get Case data in USER_MODE!
+  @wire(getCases)
+  wiredCases({ data, error }) {
+    if(data) { // creates a clickable link for Contacts
+      this.cases = data.map(item => ({
+        ...item,
+        contactUrl: item.ContactId ? '/' + item.ContactId : null,
+        caseUrl: '/' + item.Id
+      }));
+      console.log('data received from wire:', data)
+    }
+    else if(error) {
+      console.error("Wire error:", error);
+    }
+  };
+  
+  // 2. Get the default Record Type ID for Case
+  @wire(getObjectInfo, { objectApiName: CASE_OBJECT })
+  objectInfo({ data, error }) {
+    if (data) {
+      this.caseObjectInfo = data;
+      this.caseRecordTypeId = data.defaultRecordTypeId;
+      
+      // Process record types
+      const rtis = this.caseObjectInfo.recordTypeInfos;
+      // The recordTypeInfos is a map, so iterate over the keys (record type IDs)
+      this.recordTypeOptions = Object.keys(rtis).map(typeId => ({
+        label: rtis[typeId].name,
+        value: rtis[typeId].name,
+        id: typeId // metadata
+      }));
+      console.log('our record type options are', this.recordTypeOptions);
+    }
+    else if (error) {
+      console.error('Error fetching case object info', error);
+    }
+  }
+  
+  // 3. Get the picklist values using the Record Type ID and Field Reference
+  @wire(getPicklistValues, { 
+    recordTypeId: '$caseRecordTypeId', 
+    fieldApiName: PRIORITY_FIELD 
+  })
+  wiredPriorityPicklist({ data, error }) {
+    if (data) {
+      this.priorityOptions = data.values;
+      console.log('Priority Values:', this.priorityOptions);
+    } else if (error) {
+      console.error('Error fetching priority picklist values', error);
+    }
+  }
+  
+  @wire(getPicklistValues, { 
+    recordTypeId: '$caseRecordTypeId', 
+    fieldApiName: STATUS_FIELD 
+  })
+  wiredStatusPicklist({ data, error }) {
+    if (data) {
+      this.statusOptions = data.values;
+      console.log('Priority Values:', this.statusOptions);
+    } else if (error) {
+      console.error('Error fetching status picklist values', error);
+    }
+  }
+}
--- force-app/main/default/lwc/caseList/caseList.js
+++ force-app/main/default/lwc/caseList/caseList.js
@@ -2,26 +2,24 @@
 // Date: 02/04/2026
 // Desc: A dynamic list that displays Case data which can be filtered via typing in values (ex. name) or picking values (ex. picklist)
 // Dynamic Data Table base code credit: https://medium.com/@gadige.sfdc/lwc-data-table-with-dynamic-filters-28387a11cab4
-import { LightningElement, wire, track } from 'lwc';
-
-import CASE_OBJECT from '@salesforce/schema/Case';
-import STATUS_FIELD from '@salesforce/schema/Case.Status';
-import PRIORITY_FIELD from '@salesforce/schema/Case.Priority';
-
-import { getPicklistValues } from 'lightning/uiObjectInfoApi';
-import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-
-import getCases from '@salesforce/apex/Case_Manager.getCases';
+import { LightningElement, api, track } from 'lwc';
 
 export default class DynamicDataTable extends LightningElement {
-  // Sample Data
+  // Data from caseConsole LWC
+  @api cases = [];
+  @api priorityOptions = [];
+  @api statusOptions = [];
+  @api recordTypeOptions = [];
+  
+  // Updateable data
   @track tableData = [];
   @track filteredData = [];
   @track slicedData = [];
   @track filters = { status: '', priority: '', recordType: '', contact: '' };
-  @track index = 0; // used for limiting the number of records shown in the list
-  @track increment = 10;
   @track filteredDataCount;
+
+  index = 0; // used for limiting the number of records shown in the list
+  increment = 10;
   
   // Columns Definition
   columns = [
@@ -31,7 +29,7 @@ export default class DynamicDataTable extends LightningElement {
         target: '_blank'
       }
     },
-    { label: 'Status', fieldName: 'Status', type: 'text', editable: true },
+    { label: 'Status', fieldName: 'Status', type: 'text' },
     { label: 'Priority', fieldName: 'Priority', type: 'text' },
     { label: 'Record Type', fieldName: 'RecordTypeName', type: 'text' },
     { label: 'Contact', fieldName: 'contactUrl', type: 'url',
@@ -42,87 +40,27 @@ export default class DynamicDataTable extends LightningElement {
     },
     { label: 'SLA Target', fieldName: 'SLA_Target__c', type: 'date' }
   ];
-  
-  @wire(getCases)
-  wiredCases({ data, error }) {
-    if(data) { // creates a clickable link for Contacts
-      this.tableData = data.map(item => ({
-        ...item,
-        contactUrl: item.ContactId ? '/' + item.ContactId : null,
-        caseUrl: '/' + item.Id
-      }));
-      console.log('data received from wire:', data)
-      this.filteredData = [...this.tableData];
-      console.log('filtered data:', this.filteredData);
 
-      // Set to priority filter to high upon page load
-      this.filters.priority = "High";
+  // Runs as a constructor, fetching data from the parent 
+  connectedCallback() {
+    this.tableData = [...this.cases];
+    this.filteredData = [...this.tableData];
+    console.log('filtered data:', this.filteredData);
+
+    // Set the priority filter to high upon page load
+    this.filters.priority = "High";
+    this.applyFilters();
+
+    this.filteredDataCount = this.filteredData.length;
+  }
+  
+  // Reacts when the parent (caseConsole.js) data changes
+  // for example, a reloaded SOQL query button
+  renderedCallback() {
+    if (this.tableData.length !== this.cases.length) {
+      this.tableData = [...this.cases];
       this.applyFilters();
-
-      this.filteredDataCount = this.filteredData.length;
     }
-    else if(error) {
-      console.error("Wire error:", error);
-    }
-  };
-  
-  
-  // 1. Declare object variables
-  caseObjectInfo
-  caseRecordTypeId;
-  priorityOptions;
-  statusOptions;
-  recordTypeOptions;
-  
-  // 2. Get the default Record Type ID for Case
-  @wire(getObjectInfo, { objectApiName: CASE_OBJECT })
-  objectInfo({ data, error }) {
-    if (data) {
-      this.caseObjectInfo = data;
-      this.caseRecordTypeId = data.defaultRecordTypeId;
-      this.processRecordTypes();
-    }
-    else if (error) {
-      console.error('Error fetching case object info', error);
-    }
-  }
-  
-  // 3. Get the picklist values using the Record Type ID and Field Reference
-  @wire(getPicklistValues, { 
-    recordTypeId: '$caseRecordTypeId', 
-    fieldApiName: PRIORITY_FIELD 
-  })
-  priorityPicklistValues({ data, error }) {
-    if (data) {
-      this.priorityOptions = data.values;
-      console.log('Priority Values:', this.priorityOptions);
-    } else if (error) {
-      console.error('Error fetching priority picklist values', error);
-    }
-  }
-  
-  @wire(getPicklistValues, { 
-    recordTypeId: '$caseRecordTypeId', 
-    fieldApiName: STATUS_FIELD 
-  })
-  statusPicklistValues({ data, error }) {
-    if (data) {
-      this.statusOptions = data.values;
-      console.log('Priority Values:', this.statusOptions);
-    } else if (error) {
-      console.error('Error fetching status picklist values', error);
-    }
-  }
-  
-  processRecordTypes() {
-    const rtis = this.caseObjectInfo.recordTypeInfos;
-    // The recordTypeInfos is a map, so iterate over the keys (record type IDs)
-    this.recordTypeOptions = Object.keys(rtis).map(typeId => ({
-      label: rtis[typeId].name,
-      value: rtis[typeId].name,
-      id: typeId // metadata
-    }));
-    console.log('our record type options are', this.recordTypeOptions);
   }
   
   // Used for pagination of the lwc datatable
@@ -155,12 +93,12 @@ export default class DynamicDataTable extends LightningElement {
   }
 
   get prevButtonDisabled() {
-    return this.index === 0;
+    return this.index === 0; // true if we are at page 1
   }
 
   get nextButtonDisabled() {
     const maxIndex = Math.floor((this.filteredData.length - 1) / this.increment);
-    return this.index >= maxIndex;
+    return this.index >= maxIndex; // true if we are at the final page
   }
   
   // Handle Filter Change
